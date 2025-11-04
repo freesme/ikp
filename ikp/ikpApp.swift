7fe524eb453923c1db1b436d92523500a7826016
Krane <freesme@163.com>
2025-11-04 23:37:47 +0800

    Initial Commit

---
+++ ikp/ikpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ikpApp.swift
+//  ikp
+//
+//  Created by ibuprofen on 2025/11/4.
+//
+
+import SwiftUI
+
+@main
+struct ikpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
